66bfdb95d31a67b0274a63b58e64d2d35bb3136e
zongxi <ccharlesjin@gmail.com>
2024-08-30 22:40:27 +0930

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './components/sign-in/SignIn';
+import Dashboard from './components/dashboard/Dashboard'
 const App = () => {
   const [message, setMessage] = useState('');
 
@@ -15,9 +17,16 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1>{message}</h1>
-    </div>
+    // <div>
+    //   <h1>{message}</h1>
+    //   <SignIn />
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
